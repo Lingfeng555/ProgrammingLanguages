2363ec7c27e3febdf904f202c4f2c428363c92a5
Lingfeng555 <121032780+Lingfeng555@users.noreply.github.com>
2023-11-16 19:43:06 +0100

    LoadDataScript

--- src/Data/LoadData.r
+++ src/Data/LoadData.r
@@ -1,5 +1,5 @@
-stackOverFlow <- read.csv(
-    file = "Datasets/survey_results_public.csv",
+issues <- read.csv(
+    file = "Datasets/issues.csv",
     head = TRUE,
     sep = ",",
     quote = "\"'",
@@ -7,4 +7,35 @@ stackOverFlow <- read.csv(
     na.strings = "NA",
     fill = TRUE
 )
-view(stackOverFlow)
+stackOverFlow <- read.csv(
+  file = "Datasets/survey_results_public.csv",
+  head = TRUE,
+  sep = ",",
+  dec = ".",
+  na.strings = "NA",
+  fill = TRUE
+)
+popularity <- read.csv(
+  file = "Datasets/PopularityProgrammingLanguages2004to2023.csv",
+  head = TRUE,
+  sep = ",",
+  dec = ".",
+  na.strings = "NA",
+  fill = TRUE
+)
+githubRepos <- read.csv(
+  file = "Datasets/repos.csv",
+  head = TRUE,
+  sep = ",",
+  dec = ".",
+  na.strings = "NA",
+  fill = TRUE
+)
+prs <- read.csv(
+  file = "Datasets/prs.csv",
+  head = TRUE,
+  sep = ",",
+  dec = ".",
+  na.strings = "NA",
+  fill = TRUE
+)
