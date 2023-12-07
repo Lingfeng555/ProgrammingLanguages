205f245af83136c25bd4bbdb1b66e9e72778ed1b
Asier Larrazabal <asier.larrazabal@opendeusto.es>
2023-12-07 21:27:04 +0100

    Popularity and most required

---
+++ Asier/MostRequired.R
@@ -0,0 +1,43 @@
+library(dplyr)
+library(lubridate)
+library(ggplot2)
+
+getColRequiredPL <- function(){
+  col <- stackOverFlow$LanguageWantToWorkWith
+  return(col)
+}
+
+createLanguagesVector <- function(col) {
+  # Check for NA values and replace with an empty string
+  col[is.na(col)] <- ""
+  
+  # Split the column using semicolon as a delimiter
+  vec <- unlist(strsplit(col, ";"))
+  UniqueVec <- unique(vec)
+  
+  counts <- numeric(length(UniqueVec))
+  
+  # Iterate over unique element names
+  for (i in seq_along(UniqueVec)) {
+    # Count occurrences and store in the 'counts' vector
+    counts[i] <- sum(vec == UniqueVec[i])
+  }
+  
+  # Return the named vector of counts
+  names(counts) <- UniqueVec
+  return(counts)
+}
+
+colRequiredPL <- getColRequiredPL()
+counts <- createLanguagesVector(colRequiredPL)
+
+values <- as.numeric(counts)
+names <- names(counts)
+
+ordered_indices <- order(values, decreasing = TRUE)
+ordered_values <- values[ordered_indices]
+first_ordered_values <- ordered_values[1:12]
+otherRequiredValues <- sum(ordered_values[13:length(ordered_values)])
+ordered_names <- names[ordered_indices]
+
+pie(c(first_ordered_values, otherRequiredValues), labels = c(ordered_names[1:12], "Others"), main = "Chorizo")
---
+++ Asier/Popularity.R
@@ -0,0 +1,45 @@
+library(dplyr)
+library(lubridate)
+library(ggplot2)
+
+getColsNamesFromPopularity <- function(){
+  cols <- colnames(popularity)
+  cols <- cols[cols != "Date"]
+  return(cols)
+}
+
+getRowFromPopularity <- function(date){
+  row <- popularity[popularity$Date == date,]
+  row <- as.numeric(row[row != date])
+  return(row)
+}
+
+selectedValuesInDF <- function(){
+  df <- data.frame(
+    Name = cols,
+    Values = getRowFromPopularity("July 2004")
+  )
+  df <- df[order(-df$Values),]
+  return(df)
+}
+
+getColsOrdered <- function(){
+  cols2<- df[,1]
+  return(cols2)
+}
+cols2 <- getColsOrdered()
+getRowFromOrderedDF <- function(){
+  row2 <- df[,2]
+  row2 <- as.numeric(row2)
+  return(row2)
+}
+
+cols <- getColsNamesFromPopularity()
+df <- selectedValuesInDF()
+cols2 <- getColsOrdered()
+values <- getRowFromOrderedDF()
+valuesDisplay <- values[1:10]
+otherValues <- values[11:length(values)]
+otherValues <- sum(otherValues)
+
+pie(c(valuesDisplay, otherValues), labels = c(cols2[1:10], "Others"), main = "Patata")
